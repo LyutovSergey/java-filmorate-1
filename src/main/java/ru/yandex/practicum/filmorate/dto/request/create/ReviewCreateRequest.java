f65b591fe81420b6122bb983a44adfcd2a7b8b16
Sergey Lyutov <psvr2020@yandex.ru>
2026-02-17 23:22:06 +0300

    add-reviews ver 1.1

--- src/main/java/ru/yandex/practicum/filmorate/dto/request/create/ReviewCreateRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/request/create/ReviewCreateRequest.java
@@ -2,11 +2,13 @@ package ru.yandex.practicum.filmorate.dto.request.create;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ReviewCreateRequest {
     @NotBlank(message = "Контент отзыва не может быть пустым.")
+    @Size(max = 2048, message = "Максимальная длина отзыва — 2048 символов.")
     private String content;
 
     @NotNull(message = "Тип отзыва должен быть указан.")
--- src/main/java/ru/yandex/practicum/filmorate/dto/request/update/ReviewUpdateRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/request/update/ReviewUpdateRequest.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.dto.request.update;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -9,6 +10,7 @@ public class ReviewUpdateRequest {
     private Long reviewId;
 
     @NotNull(message = "Контент отзыва не может быть пустым.")
+    @Size(max = 2048, message = "Максимальная длина отзыва — 2048 символов.")
     private String content;
 
     @NotNull(message = "Тип отзыва должен быть указан.")
